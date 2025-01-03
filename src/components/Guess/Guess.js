b7f267b42068c7bac2621e6be2a067aae20a966f
Jacinta Phillips <jacintaphillips14@gmail.com>
2025-01-03 22:51:43 +1100

    Exercise 1

---
+++ src/components/Guess/Guess.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Guess() {
+  const [guess, setGuess] = React.useState("");
+
+  // clear input field after submitting
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("guess submitted: ", guess);
+    setGuess("");
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        required
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+        value={guess}
+        onChange={(event) => {
+          setGuess(event.target.value);
+        }}
+      />
+    </form>
+  );
+}
+
+export default Guess;
