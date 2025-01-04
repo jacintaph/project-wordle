8efce04e0d267ed0793295fcd129b8191ca55929
Jacinta Phillips <jacintaphillips14@gmail.com>
2025-01-04 12:32:16 +1100

    ex 2 call handlePreviousGuesses function

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-function Guess() {
+function Guess({HandlePreviousGuesses}) {
   const [guess, setGuess] = React.useState("");
 
   // clear input field after submitting
   function handleSubmit(event) {
     event.preventDefault();
+    HandlePreviousGuesses(guess)
     setGuess(""); // clear input field
   }
 
@@ -22,7 +23,7 @@ function Guess() {
         title="5 letter word"
         value={guess}
         onChange={(event) => {
-          setGuess(event.target.value);
+          setGuess((event.target.value).toUpperCase());
         }}
       />
     </form>
